918539b235ff7eddb6e7107ff58012ac0d0616f7
Pythonshik0 <109079889+Pythonshik0@users.noreply.github.com>
2024-01-16 14:29:05 +0500

    good

---
+++ Project_Job/Application/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from .views import (
+    HomeProductsView,
+    ProductView,
+    CreateCheckoutSessionView,
+    SuccessView,
+    CancelView,
+)
+
+urlpatterns = [
+    path('', HomeProductsView.as_view(), name='home-page'),
+    path('success/', SuccessView.as_view(), name='success'),
+    path('cancel/', CancelView.as_view(), name='cancel'),
+    path('item/<int:pk>', ProductView.as_view(), name='detail-product'),
+    path('buy/<int:pk>', CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
+
+]
